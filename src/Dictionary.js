d9f1e71b6cc9da15a72049af809dc2513873b25f
Eljana Palodhi <elianapalodi3@gmail.com>
2023-04-10 01:57:25 +0200

    addded new components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setkeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -22,8 +24,13 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handlekeywordChange} />
+        <input
+          type="search"
+          placeholder="search..."
+          onChange={handlekeywordChange}
+        />
       </form>
+      <Results results={results} />
     </div>
   );
 }
